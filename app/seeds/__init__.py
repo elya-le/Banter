fdb14ed929e1f933733246d26bb1a88d897bd61a
Elya <elyaj.le@gmail.com>
2024-07-21 10:24:56 -0500

    fseeding order and add messages seeder

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,8 +2,8 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .servers import seed_servers, undo_servers
 from .channels import seed_channels, undo_channels
-from .messages import seed_messages, undo_messages
 from .user_server_membership import seed_user_server_memberships, undo_user_server_memberships
+from .messages import seed_messages, undo_messages
 from app.models.db import db, environment, SCHEMA
 
 seed_commands = AppGroup('seed')
